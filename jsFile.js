6cfd9612334ee1580fc6b9b1c264fc8d00f79359
mercysound <adekunleadewolemercy@gmail.com>
2023-10-12 09:14:31 +0100

    done

--- jsFile.js
+++ jsFile.js
@@ -8,13 +8,114 @@ class PlayersBluePrint {
 
 
 const create = () =>{
-  var forPOneNo = p1No.value;
   var forPOneName = p1Name.value;
+  var forPOneNo = p1No.value;
   var forPOneWing = p1Wing.value;
-  var player1 = new PlayersBluePrint(forPOneNo, forPOneName, forPOneWing)
+  var player1 = new PlayersBluePrint(forPOneName, forPOneNo,  forPOneWing)
   // alert("is woor")
   pName1.innerHTML = player1.pName
-  pNo1.innerText = player1.pNumber
-  pWing1.innerText = player1.pWing
-  // console.log(player1.pName);
+  pNo1.innerHTML = player1.pNumber
+  pWing1.innerHTML = player1.pWing
+  //
+var forPTwoName = p2Name.value;
+var forPTwoNo = p2No.value;
+var forPTwoWing = p2Wing.value;
+var player2 = new PlayersBluePrint(forPTwoName, forPTwoNo, forPTwoWing)
+// alert("is woor")
+pNam2.innerHTML = player2.pName
+pNo2.innerHTML = player2.pNumber
+pWing2.innerHTML = player2.pWing
+console.log(player2);
+  //
+var forPThreeName = p3Name.value;
+var forPThreeNo = p3No.value;
+var forPThreeWing = p3Wing.value;
+var player3 = new PlayersBluePrint(forPThreeName, forPThreeNo, forPThreeWing)
+// alert("is woor")
+pName3.innerHTML = player3.pName
+pNo3.innerHTML = player3.pNumber
+pWing3.innerHTML = player3.pWing
+console.log(player2);
+  //
+var forPFourName = p4Name.value;
+var forPFourNo = p4No.value;
+var forPFourWing = p4Wing.value;
+var player4 = new PlayersBluePrint(forPFourName, forPFourNo, forPFourWing)
+// alert("is woor")
+pName4.innerHTML = player4.pName
+pNo4.innerHTML = player4.pNumber
+pWing4.innerHTML = player4.pWing
+console.log(player2);
+  //
+var forPFiveName = p5Name.value;
+var forPFiveNo = p5No.value;
+var forPFiveWing = p5Wing.value;
+var player5 = new PlayersBluePrint(forPFiveName, forPFiveNo, forPFiveWing)
+// alert("is woor")
+pName5.innerHTML = player5.pName
+pNo5.innerHTML = player5.pNumber
+pWing5.innerHTML = player5.pWing
+console.log(player2);
+  //
+var forPSixName = p6Name.value;
+var forPSixNo = p6No.value;
+var forPSixWing = p6Wing.value;
+var player6 = new PlayersBluePrint(forPSixName, forPSixNo, forPSixWing)
+// alert("is woor")
+pName6.innerHTML = player6.pName
+pNo6.innerHTML = player6.pNumber
+pWing6.innerHTML = player6.pWing
+console.log(player2);
+  //
+var forPSevenName = p7Name.value;
+var forPSevenNo = p7No.value;
+var forPSevenWing = p7Wing.value;
+var player7 = new PlayersBluePrint(forPSevenName, forPSevenNo, forPSevenWing)
+// alert("is woor")
+pName7.innerHTML = player7.pName
+pNo7.innerHTML = player7.pNumber
+pWing7.innerHTML = player7.pWing
+console.log(player2);
+  //
+//
+var forPEightName = p10Name.value;
+var forPEightNo = p10No.value;
+var forPEightWing = p10Wing.value;
+var player8 = new PlayersBluePrint(forPEightName, forPEightNo, forPEightWing)
+// alert("is woor")
+pName8.innerHTML = player8.pName
+pNo8.innerHTML = player8.pNumber
+pWing8.innerHTML = player8.pWing
+console.log(player8);
+//
+var forPNineName = p9Name.value;
+var forPNineNo = p9No.value;
+var forPNineWing = p9Wing.value;
+var player9 = new PlayersBluePrint(forPNineWing, forPNineNo, forPNineName)
+// alert("is woor")
+pName9.innerHTML = player9.pName
+pNo9.innerHTML = player9.pNumber
+pWing9.innerHTML = player9.pWing
+console.log(player9);
+//
+var forPTenName = p10Name.value;
+var forPTenNo = p10No.value;
+var forPTenWing = p10Wing.value;
+var player10 = new PlayersBluePrint(forPTenName, forPTenNo, forPTenWing)
+// alert("is woor")
+pName10.innerHTML = player10.pName
+pNo10.innerHTML = player10.pNumber
+pWing10.innerHTML = player10.pWing
+console.log(player10);
+
+//
+var forPElevenName = p10Name.value;
+var forPElevenNo = p10No.value;
+var forPElevenWing = p10Wing.value;
+var player11 = new PlayersBluePrint(forPElevenName, forPElevenNo, forPElevenWing)
+// alert("is woor")
+pName11.innerHTML = player11.pName
+pNo11.innerHTML = player10.pNumber
+pWing11.innerHTML = player11.pWing
+console.log(player11);
 }
